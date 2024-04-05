aec786e2593a24d3156de4f348e798242a693700
BORREANI Théo <theoborreani.toba@gmail.com>
2024-04-05 09:49:25 +0200

    initial commit

---
+++ randonneesAdventura/src/main/java/fr/miage/randonneesAdventura/RandonneesAdventuraApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.randonneesAdventura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandonneesAdventuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandonneesAdventuraApplication.class, args);
+	}
+
+}
